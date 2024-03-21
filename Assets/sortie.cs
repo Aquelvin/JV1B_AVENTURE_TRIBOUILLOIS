581521f2060eadba3a6295ae6c3c4c33b6356505
Doerys <kevtribouillois@gmail.com>
2024-03-21 17:32:47 +0100

    changement de scene prêt à usage

--- Assets/sortie.cs
+++ Assets/sortie.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class secondescene : MonoBehaviour
+public class sortie : MonoBehaviour
 {
     public BoxCollider2D bc;
 
@@ -23,7 +23,7 @@ public class secondescene : MonoBehaviour
     {
         if (other.IsTouchingLayers(3))
         {
-            SceneManager.LoadScene(2);
+            SceneManager.LoadScene(1);
         }
     }
 }
