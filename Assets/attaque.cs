d3d33730ca2b259f5dc099d3341792f54f1bcf46
Aquelvin <kevtribouillois@gmail.com>
2024-04-03 15:33:15 +0200

    tuer ennemie

---
+++ Assets/attaque.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attaque : MonoBehaviour
+{
+    public BoxCollider2D bcd;
+
+
+    [SerializeField]
+    private BoxCollider2D bc2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
+
