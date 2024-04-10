0ca447f66de1396a3d8b812b6966fc70fecd78f0
Aquelvin <kevtribouillois@gmail.com>
2024-04-10 09:45:34 +0200

    debut barre de respiration
    reste la recharger et mourir si arrive a 0

---
+++ Assets/scripts/barre_respi.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barre_respi : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Rigidbody2D rgbd;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            rgbd.velocity = new Vector2(-speed, rgbd.velocity.y);
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+    }
+
+}
