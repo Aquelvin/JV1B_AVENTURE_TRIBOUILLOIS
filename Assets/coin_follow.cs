02456d1163e892a8c9a296459de1f705fdf20d9d
MathisVchia <kevtribouillois@gmail.com>
2024-04-26 11:32:17 +0200

    jouable

---
+++ Assets/coin_follow.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin_follow : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Vitesse de déplacement de l'ennemi
+
+    private Transform boss; // Référence au joueur
+
+    private void Start()
+    {
+        boss = GameObject.FindGameObjectWithTag("mob").transform; // Trouver le joueur
+    }
+
+    private void Update()
+    {
+        if (boss != null)
+        {
+            // Calculer la direction du mouvement vers le joueur
+            Vector2 direction = boss.position - transform.position;
+            direction.Normalize();
+
+            // Appliquer le mouvement
+            transform.Translate(direction * moveSpeed * Time.deltaTime);
+        }
+    }
+}
